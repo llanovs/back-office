44e9cb8771328ba71b82ab6822691805adf69929
Liliia Lanovska <Liliia.Lanovska@netent.com>
2021-04-26 00:05:28 +0300

    added footer with picture and added build.yml to allow CICD on PRs

--- src/frontend/src/menu/RenderFooter.js
+++ src/frontend/src/menu/RenderFooter.js
@@ -1,10 +1,16 @@
-import {Layout} from "antd";
+import {
+    Layout,
+    Image
+} from "antd";
+
 import React from "react";
 
 const {Footer} = Layout;
 
 export const RenderFooter = () => {
     return <Footer style={{textAlign: "center"}}>
-        Quickhelper Backoffice
+        <Image width={75}
+               src="https://user-images.githubusercontent.com/45741777/116008803-10252980-a61f-11eb-9282-1690aead9d39.png"/>
+        <b>Quickhelper Backoffice</b>
     </Footer>;
 }
